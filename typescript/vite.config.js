21508cf12e69af02a92f344f8a3f3e320b8585e9
Wilbur <office.wilbur@gmail.com>
2025-08-03 18:37:03 -0400

    - some updates

--- typescript/vite.config.js
+++ typescript/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // HTTPS Configuration
 // uncomment the line below to enable HTTPS
-const USE_HTTPS = true;
+const USE_HTTPS = false;
 
 export default defineConfig({
     plugins: [react()],
